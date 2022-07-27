b3c9f80e91bb7c17317a2a5bf1e37148f40f3a07
Somdeb Paul <somdeb.paul1991@hotmail.com>
2022-07-27 17:27:27 +0530

    Created evenRandNo() in the randGen.class

--- src/com/science/anarky/randGen.java
+++ src/com/science/anarky/randGen.java
@@ -22,7 +22,7 @@ public class randGen {
             if (getNumericValue(userInput.charAt(n)) % 2 == 0) {
                 randNum.add(oddRandNo());
             } else if (getNumericValue(userInput.charAt(n)) % 2 != 0) {
-                randNum.add(rand.nextInt(9));
+                randNum.add(evenRandNo());
             }
         }
         String stringRandNum = null;
@@ -38,6 +38,13 @@ public class randGen {
             x = rand.nextInt(9);
         }
         return x;
+    }
+    public static int evenRandNo() {
+        int z = rand.nextInt(9);
+        while(z%2 != 0) {
+            z = rand.nextInt(9);
+        }
+        return z;
     }
 }
 
